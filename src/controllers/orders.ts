03752aa482389e18185fa09cea9ffb1fd4af89b9
JesseZeph <jessezephyr@gmail.com>
2024-09-29 00:57:15 +0100

    feat: create, cancel and get orders

---
+++ src/controllers/orders.ts
@@ -0,0 +1,116 @@
+import { Request, Response } from "express";
+import { prismaClient } from "..";
+import { NotFoundException } from "../exceptions/not-found";
+import { ErrorCode } from "../exceptions/root";
+
+export const createOrder = async (req: Request, res: Response) => {
+  return await prismaClient.$transaction(async (tx) => {
+    const cartItems = await tx.cartItem.findMany({
+      where: {
+        userId: req.user.id,
+      },
+      include: {
+        product: true,
+      },
+    });
+    if (cartItems.length == 0) {
+      return res.json({ message: "Cart is empty" });
+    }
+    const price = cartItems.reduce((prev, current) => {
+      return prev + current.quantity * +current.product.price;
+    }, 0);
+    if (!req.user.defaultShippingAddressId) {
+      return new NotFoundException(
+        "No default shipping address found",
+        ErrorCode.ADDRESS_NOT_FOUND
+      );
+    }
+    const address = await tx.address.findFirst({
+      where: {
+        id: req.user.defaultShippingAddressId,
+      },
+    });
+
+    if (!address) {
+      return new NotFoundException(
+        "Address not found",
+        ErrorCode.ADDRESS_NOT_FOUND
+      );
+    }
+    const order = await tx.order.create({
+      data: {
+        userId: req.user.id,
+        netAmount: price,
+        address: address.formattedAddress,
+        products: {
+          create: cartItems.map((cart) => {
+            return {
+              productId: cart.productId,
+              quantity: cart.quantity,
+            };
+          }),
+        },
+      },
+    });
+    const orderEvent = await tx.orderEvent.create({
+      data: {
+        orderId: order.id,
+      },
+    });
+    await tx.cartItem.deleteMany({
+      where: {
+        userId: req.user.id,
+      },
+    });
+    return res.json(order);
+  });
+};
+
+export const listOrders = async (req: Request, res: Response) => {
+  const orders = await prismaClient.order.findMany({
+    where: {
+      userId: req.user.id,
+    },
+  });
+  res.json(orders);
+};
+
+export const cancelOrder = async (req: Request, res: Response) => {
+  try {
+    const order = await prismaClient.order.update({
+      where: {
+        id: +req.params.id,
+      },
+      data: {
+        status: "CANCELLED",
+      },
+    });
+    await prismaClient.orderEvent.create({
+      data: {
+        orderId: order.id,
+        status: "CANCELLED",
+      },
+    });
+
+    res.json(order);
+  } catch (error) {
+    throw new NotFoundException("Order not found", ErrorCode.ORDER_NOT_FOUND);
+  }
+};
+
+export const getOrderById = async (req: Request, res: Response) => {
+  try {
+    const order = await prismaClient.order.findFirstOrThrow({
+      where: {
+        id: +req.params.id,
+      },
+      include: {
+        products: true,
+        events: true,
+      },
+    });
+    res.json(order);
+  } catch (error) {
+    throw new NotFoundException("Order not found", ErrorCode.ORDER_NOT_FOUND);
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import authRoutes from "./auth";
 import productRoutes from "./product";
 import usersRoutes from "./user";
+import cartRoutes from "./cart";
+import orderRoutes from "./orders";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
 rootRouter.use("/products", productRoutes);
 rootRouter.use("/users", usersRoutes);
+rootRouter.use("/carts", cartRoutes);
+rootRouter.use("/orders", orderRoutes);
 
 export default rootRouter;
